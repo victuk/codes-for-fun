5a65c2fbfa48728009bf5e30e94d3ccaae927f7d
Victor Peter <ukokjnr@gmail.com>
2020-06-30 12:13:33 +0100

    Added feature to accept whitespace when you input string with whitespace...

--- challenge.py
+++ challenge.py
@@ -2,10 +2,10 @@
 import re
 
 def rearrangedString(rearrange):
-    theRegex = re.compile(r'[abcdefghijklmnopqrstuvwxyz]', re.I)
+    theRegex = re.compile(r'[abcdefghijklmnopqrstuvwxyz\s]', re.I)
     strInRearrange = theRegex.findall(rearrange)
     strInRearrange.reverse()
     finalString =''.join(strInRearrange)
     return finalString
 
-print(rearrangedString("8o*l=l++e9H"))
+print(rearrangedString("234d%%%**&l#$%r@o))9((W 8o*l=l++e9H"))
